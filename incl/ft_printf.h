895b7db4896fbf220a8d7c1d79348264a41531f5
Ellie <102157178+Otazka@users.noreply.github.com>
2024-06-18 01:18:01 +0200

    prototypes

--- incl/ft_printf.h
+++ incl/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: elenasurovtseva <elenasurovtseva@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 15:18:15 by elenasurovt       #+#    #+#             */
-/*   Updated: 2024/06/18 00:47:58 by elenasurovt      ###   ########.fr       */
+/*   Updated: 2024/06/18 01:17:27 by elenasurovt      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,11 @@
 # include <stddef.h>
 # include <limits.h>
 
-int		    ft_hex_length(int n);
-int		    ft_nbr_length(int n);
-int		    ft_unsigned_nbr_length(unsigned int n);
-int		    ft_convert_hex(size_t nb);
-int		    ft_convert_hex_upper(unsigned int nb);
-void        ft_pointer(size_t pointer, int *length);
-void        ft_convert_unsigned_int(unsigned int u, int *length);
-int		    ft_convert_string(va_list arg);
-void        ft_hexadecimal(unsigned int x, int *length, char x_or_x);
-void        ft_putcharacter_length(char character, int *length);
-static void ft_chose_convert(char s, va_list *args, int *len, int *i);
-int		    ft_printf(const char *format, ...);
-void	    ft_unsigned_putnbr_fd(unsigned int n, int fd);
+void			ft_putcharacter_length(char character, int *length);
+void			ft_string(char *args, int *length);
+void			ft_number(int number, int *length);
+void			ft_hexadecimal(unsigned int x, int *length, char x_or_x);
+void			ft_unsigned_int(unsigned int u, int *length);
+void			ft_pointer(size_t pointer, int *length);
+int				ft_printf(const char *string, ...);
 #endif
