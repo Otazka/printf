5f19aa15c3c27106cfd10359776e3325430c5b0f
Ellie <102157178+Otazka@users.noreply.github.com>
2024-06-18 01:16:10 +0200

    all

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: elenasurovtseva <elenasurovtseva@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 15:17:22 by elenasurovt       #+#    #+#             */
-/*   Updated: 2024/06/18 01:11:35 by elenasurovt      ###   ########.fr       */
+/*   Updated: 2024/06/18 01:15:33 by elenasurovt      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "incl/ft_printf.h"
 
-int	ft_printf(const char *format, ...)
+int	ft_printf(const char *string, ...)
 {
 	va_list	args;
 	int		i;
@@ -20,18 +20,18 @@ int	ft_printf(const char *format, ...)
 
 	i = 0;
 	length = 0;
-	va_start(args, format;
-	while (format[i] != '\0')
+	va_start(args, string);
+	while (string[i] != '\0')
 	{
-		if (format[i] == '%')
+		if (string[i] == '%')
 		{
 			i++;
-			ft_printf_checker(format[i], &args, &length, &i);
+			ft_printf_checker(string[i], &args, &length, &i);
 			i++;
 		}
 		else
 		{
-			ft_putcharacter_length((char)format[i], &length);
+			ft_putcharacter_length((char)string[i], &length);
 			i++;
 		}
 	}
